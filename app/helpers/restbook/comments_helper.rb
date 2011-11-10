be7c2766ef99c8b70b979bb51708f77aacb14672
Tom Jakubowski <tom@crystae.net>
2011-11-09 17:36:20 -0800

    Added max_width option to wrap helper.

--- app/helpers/restbook/comments_helper.rb
+++ app/helpers/restbook/comments_helper.rb
@@ -1,13 +1,13 @@
 module Restbook
   module CommentsHelper
     # The Rails tutorial has a very nice wrap helper, so I'll use it too.
-    def wrap(content)
-      sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
+    def wrap(content, max_width=30)
+      sanitize(raw(content.split.map{ |s| wrap_long_string(s,max_width) }.join(' ')))
     end
 
     private
 
-    def wrap_long_string(text, max_width = 30)
+    def wrap_long_string(text, max_width=30)
       zero_width_space = "&#8203;"
       regex = /.{1,#{max_width}}/
       (text.length < max_width) ? text :
